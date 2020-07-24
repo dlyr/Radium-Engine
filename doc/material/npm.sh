e40f7f08ca0011325b2e4c31094a5e2f16611a60
dlyr <github@dlyr.fr>
2020-07-24 09:51:20 +0200

    temp save

---
+++ doc/material/npm.sh
@@ -0,0 +1,7 @@
+npm i material-components-web
+npm install --save-dev webpack webpack-cli webpack-dev-server css-loader sass-loader sass extract-loader file-loader
+
+
+npm  install --save-dev autoprefixer postcss-loader
+
+npm install --save-dev @babel/core babel-loader @babel/preset-env
