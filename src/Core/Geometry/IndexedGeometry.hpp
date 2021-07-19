2f12a2ddd6a711178f1b70a7d41637fc47172e4e
Nicolas Mellado <nmellado0@gmail.com>
2021-07-19 12:59:34 +0200

    [core] Expose LayerKeyHash struct
    Allow to reuse layers hash function outside of MultiIndexedGeometry

--- src/Core/Geometry/IndexedGeometry.hpp
+++ src/Core/Geometry/IndexedGeometry.hpp
@@ -384,18 +384,19 @@ class RA_CORE_API MultiIndexedGeometry : public AttribArrayGeometry, public Util
     /// \brief Clear attributes stored as pointers
     void deepClear();
 
-    using EntryType = std::pair<bool, std::unique_ptr<GeometryIndexLayerBase>>;
+    using LayerEntryType = std::pair<bool, std::unique_ptr<GeometryIndexLayerBase>>;
 
-    struct RA_CORE_API KeyHash {
+    struct RA_CORE_API LayerKeyHash {
         std::size_t operator()( const LayerKeyType& k ) const;
     };
 
+  private:
     /// Collection of pairs <lockStatus, Indices>
     /// \note There is no natural ordering for these elements, thus
     /// we need an unordered_map. In contrast to map, transparent hashing
     /// require c++20, so we need to implement them explicitely here
     /// https://en.cppreference.com/w/cpp/container/unordered_map/find
-    std::unordered_map<LayerKeyType, EntryType, KeyHash> m_indices;
+    std::unordered_map<LayerKeyType, LayerEntryType, LayerKeyHash> m_indices;
 };
 
 /// \name Predefined index layers
