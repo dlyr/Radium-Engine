91866c84267542924fb72d2d8a25279cb63916e6
Mathias Paulin <mathias.paulin@irit.fr>
2023-02-06 11:57:31 +0100

    [core] add type demangler from typeindex.
    Do not compile after rebase... but fixed later on.

--- src/Core/Utils/TypesUtils.hpp
+++ src/Core/Utils/TypesUtils.hpp
@@ -95,6 +95,9 @@ std::false_type is_container_impl( ... );
 template <typename T>
 using is_container = decltype( detail::is_container_impl<T>( 0 ) );
 
+/// Return the human readable version of the given type name
+std::string demangleType( const std::type_index& typeName ) noexcept;
+
 // Check if a type is a container with access to its element type and number
 // adapted from https://stackoverflow.com/questions/13830158/check-if-a-variable-type-is-iterable
 namespace detail {
@@ -189,21 +192,21 @@ struct TypeList {
 // name
 inline std::string demangleType( const std::type_index& typeIndex ) noexcept {
     std::string retval = typeIndex.name();
-    removeAllInString( retval, "class " );
-    removeAllInString( retval, "struct " );
+        removeAllInString( retval, "class " );
+        removeAllInString( retval, "struct " );
     removeAllInString( retval, "__cdecl" );
     replaceAllInString( retval, "& __ptr64", "&" );
-    replaceAllInString( retval, ",", ", " );
+        replaceAllInString( retval, ",", ", " );
     replaceAllInString( retval, " >", ">" );
     replaceAllInString( retval, "__int64", "long" );
     replaceAllInString( retval, "const &", "const&" );
-    return retval;
+        return retval;
 }
 #else
 // On Linux/macos, use the C++ ABI demangler
 inline std::string demangleType( const std::type_index& typeIndex ) noexcept {
-    int error = 0;
-    std::string retval;
+        int error = 0;
+        std::string retval;
     char* name = abi::__cxa_demangle( typeIndex.name(), 0, 0, &error );
     if ( error == 0 ) { retval = name; }
     else {
@@ -211,11 +214,11 @@ inline std::string demangleType( const std::type_index& typeIndex ) noexcept {
         // error : -2 --> not a valid mangled name
         // error : other --> __cxa_demangle
         retval = std::string( "Type demangler error : " ) + std::to_string( error );
-    }
-    std::free( name );
-    removeAllInString( retval, "__1::" ); // or "::__1" ?
+        }
+        std::free( name );
+        removeAllInString( retval, "__1::" ); // or "::__1" ?
     replaceAllInString( retval, " >", ">" );
-    return retval;
+        return retval;
 }
 #endif
 template <typename T>
