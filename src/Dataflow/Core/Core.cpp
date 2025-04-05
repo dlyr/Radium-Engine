67a26510f7df09b92f7c349d8b125d08085d3381
dlyr <github@dlyr.fr>
2025-04-05 10:59:08 +0200

    [dataflow-core] Remove macro, lib init in new Core.cpp
    Lib init register factory and create port factory instance.
    DATAFLOW_LIBRARY_INITIALIZER_DECL was used only once.

---
+++ src/Dataflow/Core/Core.cpp
@@ -0,0 +1,16 @@
+#include <Dataflow/RaDataflow.hpp>
+
+#include <Dataflow/Core/Node.hpp>
+#include <Dataflow/Core/Nodes/CoreBuiltInsNodes.hpp>
+
+void fCoreNodes__Initializer();
+
+struct fCoreNodes__Initializer_t_ {
+    fCoreNodes__Initializer_t_() { ::fCoreNodes__Initializer(); }
+};
+static fCoreNodes__Initializer_t_ fCoreNodes__Initializer__;
+using namespace Ra::Dataflow::Core;
+void fCoreNodes__Initializer() {
+    NodeFactoriesManager::registerStandardFactories();
+    PortFactory::createInstance();
+}
