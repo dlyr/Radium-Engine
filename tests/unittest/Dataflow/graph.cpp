8676b72fbb02e4793c51267054c4b37707d78f8a
dlyr <github@dlyr.fr>
2025-04-09 09:31:49 +0200

    [dataflow-core] Node refactor cont'd.

--- tests/unittest/Dataflow/graph.cpp
+++ tests/unittest/Dataflow/graph.cpp
@@ -348,7 +348,7 @@ TEST_CASE( "Dataflow/Core/Graph/Node failed exeution" ) {
       public:
         explicit FailFunction( const std::string& instanceName ) : FunctionNode( instanceName ) {}
         bool execute() { return false; }
-        static const std::string& getTypename() {
+        static const std::string& node_typename() {
             static std::string demangledName = std::string { "FailFunction" };
             return demangledName;
         }
