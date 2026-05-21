f3e23a0ff5037af1039179be59d96a644181cad0
dlyr <github@dlyr.fr>
2026-05-21 11:19:52 +0200

    [core] Add missing SurfaceMeshComponent ctor.

--- src/Engine/Scene/GeometryComponent.hpp
+++ src/Engine/Scene/GeometryComponent.hpp
@@ -239,6 +239,16 @@ inline SurfaceMeshComponent::SurfaceMeshComponent( const std::string& name,
     finalizeROFromGeometry( convertMatdataToMaterial( mat ), Core::Transform::Identity() );
 }
 
+inline SurfaceMeshComponent::SurfaceMeshComponent( const std::string& name,
+                                                   Entity* entity,
+                                                   CoreMeshType&& mesh,
+                                                   std::shared_ptr<Data::Material> mat ) :
+    GeometryComponent( name, entity ),
+    m_displayMesh( new RenderMeshType( name, std::move( mesh ) ) ) {
+    setContentName( name );
+    finalizeROFromGeometry(  mat, Core::Transform::Identity() );
+}
+
 inline std::shared_ptr<Data::Material>
 SurfaceMeshComponent::convertMatdataToMaterial( const Core::Asset::MaterialData* data ) {
     // The technique for rendering this component
