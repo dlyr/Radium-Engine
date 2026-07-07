691f01c316c58eed4b368a88f325d455db829cbe
Jihao CAI <jcai@hokusai.irit.fr>
2026-07-07 10:47:21 +0200

    Stage 1: create manual point cloud

--- examples/HelloPointCloud/main.cpp
+++ examples/HelloPointCloud/main.cpp
@@ -30,20 +30,32 @@ int main( int argc, char* argv[] ) {
     // todo
     // stage 1
     // create point cloud by hand
-    
+    Ra::Core::Geometry::PointCloud pointCloud;
+    pointCloud.setVertices( { Ra::Core::Vector3( 0.0, 0.0, 0.0 ),
+                              Ra::Core::Vector3( 1.0, 0.0, 0.0 ),
+                              Ra::Core::Vector3( 0.0, 1.0, 0.0 ),
+                              Ra::Core::Vector3( 0.0, 0.0, 1.0 ) } );
+
+
+
+
+
     // stage 2
     // load point cloud from file
 
     //! [Create the engine entity for the point cloud]
     auto e = app.m_engine->getEntityManager()->createEntity( "point cloud" );
 
-
+    //todo
     // create point cloud component
-    //    auto c =
-    
+    // auto c =
+    auto c = new Ra::Engine::Scene::PointCloudComponent( "manual point cloud",
+                                                         e,
+                                                         std::move( pointCloud ) );
+    c->setSplatSize( 0.05f );
     //! [Register the entity/component association to the geometry system ]
-    //auto geometrySystem = app.m_engine->getSystem( "GeometrySystem" );
-      //    geometrySystem->addComponent( e, c );
+    auto geometrySystem = app.m_engine->getSystem( "GeometrySystem" );
+        geometrySystem->addComponent( e, c );
     //! [Register the entity/component association to the geometry system ]
 
     //! [Tell the window that something is to be displayed]
