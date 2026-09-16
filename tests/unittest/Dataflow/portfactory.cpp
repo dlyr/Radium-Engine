a12c8a3ce9910d70593e783457e3dd0172bad75d
dlyr <github@dlyr.fr>
2026-09-16 09:41:07 +0200

    [tests] Uptade dataflow unit tests.

--- tests/unittest/Dataflow/portfactory.cpp
+++ tests/unittest/Dataflow/portfactory.cpp
@@ -13,47 +13,65 @@ TEST_CASE( "Dataflow/Core/PortFactory", "[unittests][Dataflow][PortFactory]" ) {
     auto factory = PortFactory::getInstance();
 
     SECTION( "After init default port types are available" ) {
-        REQUIRE( factory->output_setter( std::type_index( typeid( Scalar ) ) ) );
-        REQUIRE( factory->output_setter( std::type_index( typeid( int ) ) ) );
-        REQUIRE( factory->output_setter( std::type_index( typeid( unsigned int ) ) ) );
-        REQUIRE( factory->output_setter( std::type_index( typeid( Utils::Color ) ) ) );
-        REQUIRE( factory->output_setter( std::type_index( typeid( Vector2 ) ) ) );
-        REQUIRE( factory->output_setter( std::type_index( typeid( Vector3 ) ) ) );
-        REQUIRE( factory->output_setter( std::type_index( typeid( Vector4 ) ) ) );
-        REQUIRE( factory->input_getter( std::type_index( typeid( Scalar ) ) ) );
-        REQUIRE( factory->input_getter( std::type_index( typeid( int ) ) ) );
-        REQUIRE( factory->input_getter( std::type_index( typeid( unsigned int ) ) ) );
-        REQUIRE( factory->input_getter( std::type_index( typeid( Utils::Color ) ) ) );
-        REQUIRE( factory->input_getter( std::type_index( typeid( Vector2 ) ) ) );
-        REQUIRE( factory->input_getter( std::type_index( typeid( Vector3 ) ) ) );
-        REQUIRE( factory->input_getter( std::type_index( typeid( Vector4 ) ) ) );
+        REQUIRE( factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<Scalar>() ) );
+        REQUIRE( factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<int>() ) );
+        REQUIRE(
+            factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<unsigned int>() ) );
+        REQUIRE(
+            factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<Utils::Color>() ) );
+        REQUIRE( factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<Vector2>() ) );
+        REQUIRE( factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<Vector3>() ) );
+        REQUIRE( factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<Vector4>() ) );
+        REQUIRE( factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<Scalar>() ) );
+        REQUIRE( factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<int>() ) );
+        REQUIRE(
+            factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<unsigned int>() ) );
+        REQUIRE(
+            factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<Utils::Color>() ) );
+        REQUIRE( factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<Vector2>() ) );
+        REQUIRE( factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<Vector3>() ) );
+        REQUIRE( factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<Vector4>() ) );
     }
 
     SECTION( "Other types aren't available" ) {
-        REQUIRE_THROWS( factory->output_setter( std::type_index( typeid( std::string ) ) ) );
-        REQUIRE_THROWS( factory->output_setter( std::type_index( typeid( std::vector<int> ) ) ) );
-        REQUIRE_THROWS( factory->input_getter( std::type_index( typeid( std::string ) ) ) );
-        REQUIRE_THROWS( factory->input_getter( std::type_index( typeid( std::vector<int> ) ) ) );
+        REQUIRE_THROWS(
+            factory->output_setter( Ra::Core::Utils::simplifiedDemangledType<std::string>() ) );
+        REQUIRE_THROWS( factory->output_setter(
+            Ra::Core::Utils::simplifiedDemangledType<std::vector<int>>() ) );
+        REQUIRE_THROWS(
+            factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<std::string>() ) );
+        REQUIRE_THROWS(
+            factory->input_getter( Ra::Core::Utils::simplifiedDemangledType<std::vector<int>>() ) );
     }
 
     auto node = Sources::SingleDataSourceNode<int>( "node" );
 
     SECTION( "Can add port with default types" ) {
-        REQUIRE( factory->make_input_port( &node, "port", typeid( Scalar ) ) );
-        REQUIRE( factory->make_output_port( &node, "port", typeid( Scalar ) ) );
+        REQUIRE( factory->make_input_port(
+            &node, "port", Ra::Core::Utils::simplifiedDemangledType<Scalar>() ) );
+        REQUIRE( factory->make_output_port(
+            &node, "port", Ra::Core::Utils::simplifiedDemangledType<Scalar>() ) );
     }
     SECTION( "Can't add port with other types" ) {
-        REQUIRE( !factory->make_input_port( &node, "port0", typeid( std::string ) ) );
-        REQUIRE( !factory->make_output_port( &node, "port0", typeid( std::string ) ) );
-        REQUIRE( !factory->make_output_port( &node, "port1", typeid( std::vector<int> ) ) );
-        REQUIRE( !factory->make_input_port( &node, "port1", typeid( std::vector<int> ) ) );
+        REQUIRE( !factory->make_input_port(
+            &node, "port0", Ra::Core::Utils::simplifiedDemangledType<std::string>() ) );
+        REQUIRE( !factory->make_output_port(
+            &node, "port0", Ra::Core::Utils::simplifiedDemangledType<std::string>() ) );
+        REQUIRE( !factory->make_output_port(
+            &node, "port1", Ra::Core::Utils::simplifiedDemangledType<std::vector<int>>() ) );
+        REQUIRE( !factory->make_input_port(
+            &node, "port1", Ra::Core::Utils::simplifiedDemangledType<std::vector<int>>() ) );
     }
     SECTION( "Add new port type and can add port with other types" ) {
         add_port_type<std::string>();
         add_port_type<std::vector<int>>();
-        REQUIRE( factory->make_input_port( &node, "port0", typeid( std::string ) ) );
-        REQUIRE( factory->make_output_port( &node, "port0", typeid( std::string ) ) );
-        REQUIRE( factory->make_output_port( &node, "port1", typeid( std::vector<int> ) ) );
-        REQUIRE( factory->make_input_port( &node, "port1", typeid( std::vector<int> ) ) );
+        REQUIRE( factory->make_input_port(
+            &node, "port0", Ra::Core::Utils::simplifiedDemangledType<std::string>() ) );
+        REQUIRE( factory->make_output_port(
+            &node, "port0", Ra::Core::Utils::simplifiedDemangledType<std::string>() ) );
+        REQUIRE( factory->make_output_port(
+            &node, "port1", Ra::Core::Utils::simplifiedDemangledType<std::vector<int>>() ) );
+        REQUIRE( factory->make_input_port(
+            &node, "port1", Ra::Core::Utils::simplifiedDemangledType<std::vector<int>>() ) );
     }
 }
